e2d0926060695c45334870cd290869dbc6b329b0
AdedigbaOluwad1 <boatech4good@gmail.com>
2023-11-29 00:29:07 +0100

    Test

--- src/pages/invoice/Invoice.tsx
+++ src/pages/invoice/Invoice.tsx
@@ -15,9 +15,7 @@ function Invoice() {
     const { id } = router.query
 
     const pay = () => {
-        axios.post(
-            // process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_BASE_URL! : process.env.NEXT_PUBLIC_API_BASE_URL! + 
-            'https://klusterthon.onrender.com/api/invoices/pay', {
+        axios.post(process.env.NEXT_PUBLIC_API_BASE_URL! + '/invoices/pay', {
             id: id
         })
         .then(e => console.log(e.data))
