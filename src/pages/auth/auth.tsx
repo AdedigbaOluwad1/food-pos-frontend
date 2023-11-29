881cd70902820a1b0313fd4db7b0698d9e52ed71
AdedigbaOluwad1 <boatech4good@gmail.com>
2023-11-29 16:34:00 +0100

    Fix bug hindering deployment

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Login from './login'
 
-export default function Index() {
+export default function Auth() {
   return (
     <div className='bg-primary min-h-screen px-6 py-10 flex items-center justify-center'>
       <div className='w-full max-w-lg bg-light h-[400px] p-6 md:p-8 flex items-center' style={{
